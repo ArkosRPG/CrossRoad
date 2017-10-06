c110d28663aa0bc0a2e7c24f07d70f7b87250b9f
Arkos <arkos.game.lab@gmail.com>
2017-10-06 12:21:51 +0500

    Initial prefabs and scene

---
+++ Assets/Scripts/CrossRenderer.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public sealed class CrossRenderer : MonoBehaviour
+{
+	[SerializeField]
+	private Color _color = Color.magenta;
+	private SpriteRenderer[] _sprites;
+
+
+	void Start()
+	{
+		_sprites = GetComponentsInChildren<SpriteRenderer>();
+#if UNITY_EDITOR
+	}
+
+	void Update()
+	{
+#endif
+		UpdateColor();
+	}
+
+
+	public void UpdateColor(Color color)
+	{
+		_color = color;
+		UpdateColor();
+	}
+
+	private void UpdateColor()
+	{
+		foreach (var sprite in _sprites)
+		{
+			sprite.color = _color;
+		}
+	}
+}
