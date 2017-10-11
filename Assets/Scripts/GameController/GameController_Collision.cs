329bbabfbfe370e5acc213b71b92e4befdcba50c
Arkos <arkos.game.lab@gmail.com>
2017-10-12 04:12:11 +0500

    refactor: GameController

---
+++ Assets/Scripts/GameController/GameController_Collision.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public partial class GameController : MonoBehaviour
+{
+	private HashSet<Wrap> _crosses = new HashSet<Wrap>();
+
+
+	private void AddToCollisionList(Wrap wrap)
+	{
+		_crosses.Add(wrap);
+	}
+
+
+	private void Update_Collisions()
+	{
+		var crushers = _crosses.Where(c => c.CMC.IsCrusher());
+		var obstacles = _crosses.Where(c => c.CMC.IsObstacle());
+
+		foreach (var obstacle in obstacles)
+		{
+			foreach (var crusher in crushers)
+			{
+				var delta = obstacle.Tf.position - crusher.Tf.position;
+				var X = Mathf.Abs(delta.x);
+				var Y = Mathf.Abs(delta.y);
+
+				if ((X < Constants.CROSS_BORDER_1 && Y < Constants.CROSS_BORDER_3) ||
+					(X < Constants.CROSS_BORDER_2 && Y < Constants.CROSS_BORDER_2) ||
+					(X < Constants.CROSS_BORDER_3 && Y < Constants.CROSS_BORDER_1))
+				{
+					obstacle.CMC.ReportCollision();
+					crusher.CMC.ReportCollision();
+
+					if (_scoring)
+					{
+						_scoring = false;
+						_hiScore = Mathf.Max(_hiScore, _score);
+						PlayerPrefs.SetInt(Constants.PP_SCORE, _hiScore);
+						PlayerPrefs.Save();
+
+						StartCoroutine(RestartCoroutine());
+					}
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/GameController/GameController_Pools.cs
+++ Assets/Scripts/GameController/GameController_Pools.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -9,8 +10,7 @@ public partial class GameController : MonoBehaviour
 	[SerializeField] private GameObject _enemyPrefab;
 	[SerializeField] private Transform _root;
 
-	private HashSet<Wrap> _crosses;
-	private HashSet<Wrap> _pool;
+	private HashSet<Wrap> _pool = new HashSet<Wrap>();
 
 
 	private void InstantiatePlayer()
@@ -19,28 +19,54 @@ public partial class GameController : MonoBehaviour
 		var pc = go.GetComponent<PlayerController>();
 		pc.Init(this, MovementType.Player, Constants.PLAYER_INITIAL_STEER);
 
-		_crosses.Add(new Wrap(go, pc.transform, pc));
+		AddToCollisionList(new Wrap(go, pc.transform, pc));
 	}
 
 
-	private void InstantiateEnemy()
+	private Wrap InstantiateEnemy()
 	{
-		var go = Instantiate(_playerPrefab, new Vector3(Constants.PLAYER_INITIAL_X, Constants.PLAYER_INITIAL_Y, 0f), Quaternion.identity, _root);
-		var pc = go.GetComponent<PlayerController>();
-		pc.Init(this, MovementType.Player, Constants.PLAYER_INITIAL_STEER);
+		var go = Instantiate(_enemyPrefab, _root);
+		var cmc = go.GetComponent<CrossMovementController>();
 
-		_crosses.Add(new Wrap(go, pc.transform, pc));
+		var wrap = new Wrap(go, cmc.transform, cmc);
+		AddToCollisionList(wrap);
+
+		return wrap;
+	}
+
+
+	private void InitEnemy(Wrap wrap)
+	{
+		wrap.Tf.position = new Vector3(-Constants.BORDER_X * (Random.value > .5f ? -1f : 1f), Constants.BORDER_Y, 0f);
+		wrap.CMC.Init(this, Random.value > .5f ? MovementType.Fast : MovementType.Static);
+	}
+
+
+	private void Pull()
+	{
+		Wrap wrap;
+		if (_pool.Count > 0)
+		{
+			wrap = _pool.First();
+			wrap.GO.SetActive(true);
+			_pool.Remove(wrap);
+		}
+		else
+		{
+			wrap = InstantiateEnemy();
+		}
+		InitEnemy(wrap);
 	}
 
 
 	public void ReportFree(GameObject go, Transform tf, CrossMovementController cmc)
 	{
-		PutInPool(new Wrap(go, tf, cmc));
+		Push(new Wrap(go, tf, cmc));
 		GatherScore(cmc);
 	}
 
 
-	private void PutInPool(Wrap wrap)
+	private void Push(Wrap wrap)
 	{
 		wrap.GO.SetActive(false);
 		_pool.Add(wrap);
