031416a279d1c9a854c05146ab701f518e96831f
Arkos <arkos.game.lab@gmail.com>
2017-10-12 03:26:29 +0500

    refactor: split

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+
+public partial class PlayerController : CrossMovementController
+{
+	[Obsolete("Use with switch instead. Exists for end of game where player can be spawned as enemy.", true)]
+	public override void Init(GameController gameController, MovementType movementType)
+	{
+		base.Init(gameController, movementType);
+	}
+
+
+	public void Init(GameController gameController, MovementType movementType, Switch steering)
+	{
+		base.Init(gameController, movementType);
+		_steering = steering;
+	}
+}
